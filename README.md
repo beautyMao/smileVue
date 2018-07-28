c1aa05be9240137c43484e52ae34c8b2f61383a5
jhpanc <1284725076@qq.com>
2018-07-28 22:53:14 +0800

    代码提交

---
+++ README.md
@@ -0,0 +1,7 @@
+使用vant，
+
+    px2rem-loader，用于将单位转化为 rem，以750px设计图为基准
+    lib-flexible 用于设置 rem 基准值
+
+图片采用require的方式引入
+图片采用图片懒加载的方式进行加载
---
+++ src/components/pages/example.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+
+export default {
+  name: 'ShoppingMall',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
